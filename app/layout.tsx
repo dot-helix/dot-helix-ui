6b50426a0268e3e9b08d796ec354e67e7c1fe0fd
mimshins <mostafa.sh.coderino@gmail.com>
2023-03-26 22:27:44 +0330

    update dev sandbox

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,38 @@
 "use client";
 
 import * as React from "react";
+import { getVariablesAsInlineStyle, Initializer } from "../lib/configuration";
+import classes from "./layout.module.css";
 
-const RootLayout = (props: { children: React.ReactNode }) => (
-  <html lang="en">
-    <body>{props.children}</body>
-  </html>
-);
+const ssrCssVariables = getVariablesAsInlineStyle();
+
+const RootLayout = (props: { children: React.ReactNode }) => {
+  const [scheme, setScheme] = React.useState<"light" | "dark">("light");
+
+  const primitives = React.useMemo<
+    Parameters<typeof getVariablesAsInlineStyle>[0]
+  >(() => ({ colorScheme: scheme }), [scheme]);
+
+  return (
+    <html lang="en" style={ssrCssVariables}>
+      <body>
+        <Initializer primitives={primitives}>
+          <div className={classes.root}>
+            <div className={classes.toolbar}>
+              <button
+                onClick={() =>
+                  void setScheme(s => (s === "light" ? "dark" : "light"))
+                }
+              >
+                Toggle color scheme
+              </button>
+            </div>
+            {props.children}
+          </div>
+        </Initializer>
+      </body>
+    </html>
+  );
+};
 
 export default RootLayout;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import classes from "./page.module.css";
+
 const Page = () => {
-  return <h1>Dev Page</h1>;
+  return (
+    <div className={classes.root}>
+      <h1 className={classes.title}>Dev Sandbox</h1>
+    </div>
+  );
 };
 
 export default Page;
