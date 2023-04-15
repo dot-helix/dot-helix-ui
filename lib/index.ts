3eb775798bfbdd351163f4f8e2dbdf3976fe4b7d
mimshins <mostafa.sh.coderino@gmail.com>
2023-04-15 17:49:17 +0330

    [⭐️] add Checkbox component

--- lib/index.ts
+++ lib/index.ts
@@ -2,6 +2,8 @@ export * from "./Breadcrumb";
 export { default as Breadcrumb } from "./Breadcrumb";
 export * from "./Button";
 export { default as Button } from "./Button";
+export * from "./Checkbox";
+export { default as Checkbox } from "./Checkbox";
 export * from "./IconButton";
 export { default as IconButton } from "./IconButton";
 export * from "./LoadingIndicator";
