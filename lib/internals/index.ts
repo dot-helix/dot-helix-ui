be0f0e8a9b8cc271e8d3734f68f5a892bdae53bb
mimshins <mostafa.sh.coderino@gmail.com>
2023-05-03 20:27:25 +0330

    [⭐️] Add internal chevrons

--- lib/internals/index.ts
+++ lib/internals/index.ts
@@ -1,2 +1,6 @@
+export * from "./ChevronLeftIcon";
+export { default as ChevronLeftIcon } from "./ChevronLeftIcon";
+export * from "./ChevronRightIcon";
+export { default as ChevronRightIcon } from "./ChevronRightIcon";
 export * from "./CloseIcon";
 export { default as CloseIcon } from "./CloseIcon";
