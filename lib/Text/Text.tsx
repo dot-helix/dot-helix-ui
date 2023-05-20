65c82a3b2ea7a02cdeef861fe96b9c8fb74de5c3
mimshins <mostafa.sh.coderino@gmail.com>
2023-05-20 16:07:57 +0330

    [⭐️] Add Text component

---
+++ lib/Text/Text.tsx
@@ -0,0 +1,123 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import type { PolymorphicProps } from "@styleless-ui/react/typings";
+import cls from "classnames";
+import * as React from "react";
+import classes from "./Text.module.css";
+
+interface OwnProps {
+  /**
+   * The className applied to the component.
+   */
+  className?: string;
+  /**
+   * The content of the component.
+   */
+  children?: React.ReactNode;
+  /**
+   * Applies the theme typography styles.
+   */
+  variant:
+    | "h1"
+    | "h2"
+    | "h3"
+    | "h4"
+    | "h5"
+    | "h6"
+    | "subheading1"
+    | "subheading2"
+    | "body1"
+    | "body2"
+    | "caption";
+  /**
+   * The color of the text.
+   * @default "current"
+   */
+  color?:
+    | "current"
+    | "neutral-normal"
+    | "neutral-inverted"
+    | "neutral-secondary"
+    | "neutral-tertiary"
+    | "neutral-quaternary"
+    | "neutral-disabled"
+    | "primary"
+    | "secondary"
+    | "error"
+    | "success"
+    | "warning"
+    | "info";
+  /**
+   * Set the text-align on the text.
+   */
+  align?: "left" | "center" | "right" | "justify" | "start" | "end";
+  /**
+   * Set the font-weight on the text.
+   */
+  weight?: 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900;
+  /**
+   * Set the display on the text.
+   */
+  display?: "inline" | "block" | "inline-block" | "flex" | "inline-flex";
+  /**
+   * Set the text-overflow on the text.
+   * @default "ellipsis"
+   */
+  textOverflow?: "clip" | "ellipsis";
+  /**
+   * If `true`, the text will not wrap,
+   * but instead will truncate or clip based on the `textOverflow` prop provided.
+   *
+   * Note that text overflow can only happen
+   * when the element has a width in order to overflow.
+   * ('block', 'inline-block', 'flex', 'inline-flex')
+   * @default false
+   */
+  noWrap?: boolean;
+}
+
+export type Props<E extends React.ElementType> = PolymorphicProps<E, OwnProps>;
+
+const TextBase = <E extends React.ElementType, R extends HTMLElement>(
+  props: Props<E>,
+  ref: React.Ref<R>,
+) => {
+  const {
+    className,
+    children,
+    variant,
+    align,
+    weight,
+    display,
+    as: RootNode = "span",
+    color = "current",
+    textOverflow = "ellipsis",
+    noWrap = false,
+    ...otherProps
+  } = props;
+
+  return (
+    <RootNode
+      {...otherProps}
+      ref={ref}
+      className={cls(
+        className,
+        classes.root,
+        classes[`root--${variant}`],
+        classes[`root--${color}`],
+        classes[`root--${textOverflow}`],
+        {
+          [classes[`root--no-wrap`]!]: noWrap,
+          [classes[`root--align-${align!}`]!]: !!align,
+          [classes[`root--weight-${weight!}`]!]: !!weight,
+          [classes[`root--display-${display!}`]!]: !!display,
+        },
+      )}
+    >
+      {children}
+    </RootNode>
+  );
+};
+
+const Text = React.forwardRef(TextBase) as typeof TextBase;
+
+export default Text;
---
+++ lib/Text/index.ts
@@ -0,0 +1,1 @@
+export { default, type Props as TextProps } from "./Text";
