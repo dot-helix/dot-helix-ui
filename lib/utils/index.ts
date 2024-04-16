40c40b4adcd42f2a06c4a2a666b9a5e06860ef1d
mimshins <mostafa.sh.coderino@gmail.com>
2024-04-16 15:12:00 +0330

    Add new utility

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,3 +1,3 @@
-export { useDeterministicId } from "@styleless-ui/react/utils";
+export { isFragment, useDeterministicId } from "@styleless-ui/react/utils";
 export * from "./color";
 export { default as useConstantValue } from "./use-constant-value";
