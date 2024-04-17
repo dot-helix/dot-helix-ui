b33f7d285c99028f4699cb04c6c70545b0cd9282
mimshins <mostafa.sh.coderino@gmail.com>
2024-04-17 17:22:37 +0330

    Add new shared types

--- lib/types.ts
+++ lib/types.ts
@@ -1,4 +1,7 @@
+/* eslint-disable @typescript-eslint/ban-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 export type AnyObject = Record<keyof any, unknown>;
 export type AnyFunction = (...args: any) => any;
+
+export type EmptyObjectNotation = {};
