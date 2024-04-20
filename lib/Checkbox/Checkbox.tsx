12543e0f955437cf1c973415615b7640ebac4653
mimshins <mostafa.sh.coderino@gmail.com>
2024-04-20 12:19:43 +0330

    Refactor checkbox component

--- lib/Checkbox/Checkbox.tsx
+++ lib/Checkbox/Checkbox.tsx
@@ -2,92 +2,139 @@
 import {
   Checkbox as StylelessCheckbox,
   type CheckboxProps,
+  type MergeElementProps,
 } from "@styleless-ui/react";
 import cls from "classnames";
 import * as React from "react";
+import Label from "../Label";
+import { Logger } from "../internals";
+import type { CommonProps } from "../types";
+import { componentWithForwardedRef, useDeterministicId } from "../utils";
 import classes from "./Checkbox.module.css";
+import * as Slots from "./slots";
 
-type OwnProps = Pick<
-  CheckboxProps,
-  | "label"
-  | "value"
-  | "onChange"
-  | "checked"
-  | "defaultChecked"
-  | "disabled"
-  | "autoFocus"
-  | "indeterminated"
-> & {
-  /**
-   * The className applied to the component.
-   */
-  className?: string;
-  /**
-   * The size of the checkbox.
-   * @default "medium"
-   */
-  size?: "large" | "medium" | "small";
-  /**
-   * If `true`, the checkbox will fill the parent's width.
-   * @default false
-   */
-  fluid?: boolean;
-};
+type OwnProps = Pick<CommonProps, "className"> &
+  Pick<
+    CheckboxProps,
+    | "name"
+    | "value"
+    | "onCheckedChange"
+    | "checked"
+    | "defaultChecked"
+    | "disabled"
+    | "autoFocus"
+    | "readOnly"
+  > & {
+    /**
+     * The size of the checkbox.
+     *
+     * @default "medium"
+     */
+    size?: "large" | "medium" | "small";
+    /**
+     * If `true`, the checkbox will fill the parent's width.
+     *
+     * @default false
+     */
+    fluid?: boolean;
+    /**
+     * The visible label text of the checkbox.
+     */
+    label: string;
+  };
 
 export type Props = Omit<
-  React.ComponentPropsWithRef<"div">,
-  keyof OwnProps | "value" | "defaultValue" | "children"
-> &
-  OwnProps;
+  MergeElementProps<"div", OwnProps>,
+  "defaultValue" | "children" | "onChange" | "onChangeCapture"
+>;
 
 const CheckboxBase = (props: Props, ref: React.Ref<HTMLDivElement>) => {
   const {
+    id: idProp,
     className,
     label,
+    name,
     value,
     checked,
     defaultChecked,
-    disabled,
-    onChange,
-    indeterminated = false,
+    onCheckedChange,
+    disabled = false,
+    readOnly = false,
     fluid = false,
     autoFocus = false,
     size = "medium",
     ...otherProps
   } = props;
 
+  const scopeId = useDeterministicId(idProp, "hui-checkbox-scope");
+  const boxId = `${scopeId}__box`;
+  const labelId = `${scopeId}__label`;
+
+  if (disabled && readOnly) {
+    Logger.devOnly.log(
+      "You can't have both `disabled` and `readOnly` props set to `true`.",
+      "error",
+      "Checkbox",
+    );
+  }
+
   return (
     <div
       {...otherProps}
+      id={scopeId}
+      ref={ref}
       className={cls(className, classes.root, classes[`root--${size}`], {
         [classes["root--fluid"]!]: fluid,
       })}
-      ref={ref}
+      data-slot={Slots.Wrapper}
     >
       <StylelessCheckbox
-        label={label}
-        indeterminated={indeterminated}
-        disabled={disabled}
-        onChange={onChange}
+        id={boxId}
+        label={{ labelledBy: labelId }}
+        disabled={!readOnly && disabled}
+        readOnly={readOnly}
+        onCheckedChange={onCheckedChange}
         checked={checked}
         autoFocus={autoFocus}
         defaultChecked={defaultChecked}
         value={value}
-        classes={({ checked, disabled, indeterminated, focusedVisible }) => ({
-          root: cls(classes.input, {
+        name={name}
+        data-size={size}
+        data-fluid={fluid ? "" : undefined}
+        className={({
+          checked,
+          disabled,
+          indeterminated,
+          focusedVisible,
+          readOnly,
+        }) =>
+          cls(classes.input, {
             [classes["input--disabled"]!]: disabled,
             [classes["input--checked"]!]: checked,
             [classes["input--focus-visible"]!]: focusedVisible,
             [classes["input--indeterminated"]!]: indeterminated,
-          }),
-          check: classes.check,
-          label: classes.label,
-        })}
-      />
+            [classes["input--readonly"]!]: readOnly,
+          })
+        }
+      >
+        <div
+          aria-hidden="true"
+          className={cls(classes["check-indicator"])}
+          data-slot={Slots.CheckIndicator}
+        ></div>
+      </StylelessCheckbox>
+      <Label
+        id={labelId}
+        targetId={boxId}
+        className={classes.label}
+        data-slot={Slots.Label}
+      >
+        {label}
+      </Label>
     </div>
   );
 };
 
-const Checkbox = React.forwardRef(CheckboxBase) as typeof CheckboxBase;
+const Checkbox = componentWithForwardedRef(CheckboxBase, "Checkbox");
 
 export default Checkbox;
