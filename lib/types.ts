1f19a81a44167091072e5ef89bf96d8501358d79
mimshins <mostafa.sh.coderino@gmail.com>
2024-04-20 12:19:15 +0330

    Add common props shared type

--- lib/types.ts
+++ lib/types.ts
@@ -5,3 +5,14 @@ export type AnyObject = Record<keyof any, unknown>;
 export type AnyFunction = (...args: any) => any;
 
 export type EmptyObjectNotation = {};
+
+export type CommonProps = {
+  /**
+   * The className applied to the component.
+   */
+  className?: string;
+  /**
+   * The content of the component.
+   */
+  children?: React.ReactNode;
+};
