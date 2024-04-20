f7b96c28b4d205d212aafedd6328dea3153e5bcd
mimshins <mostafa.sh.coderino@gmail.com>
2024-04-20 16:10:07 +0330

    Update slots

--- lib/Checkbox/slots.ts
+++ lib/Checkbox/slots.ts
@@ -1,4 +1,4 @@
-export const Root = "Checkbox:Root";
+export const Root = "Checkbox:Parent:Root";
 export const Container = "Checkbox:Container";
 export const CheckIndicator = "Checkbox:CheckIndicator";
 export const RequiredIndicator = "Checkbox:RequiredIndicator";
