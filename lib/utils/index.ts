2a55794ac0bb5b0b06354ab9a1dcf83f76a58d81
mimshins <mostafa.sh.coderino@gmail.com>
2024-04-21 14:04:23 +0330

    Add new utility

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -7,3 +7,4 @@ export * from "./color";
 export { default as forkCallbacks } from "./fork-callbacks";
 export { default as hasValidityChanged } from "./has-validity-changed";
 export { default as useConstantValue } from "./use-constant-value";
+export { default as useValidityChangeEmitter } from "./use-validity-change-emitter";
