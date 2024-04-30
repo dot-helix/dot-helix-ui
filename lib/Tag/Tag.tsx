afe2a4f44bc0022c4fc217672af597107ce4f0eb
mimshins <mostafa.sh.coderino@gmail.com>
2024-04-30 22:26:16 +0330

    Refactor tag component

--- lib/Tag/Tag.tsx
+++ lib/Tag/Tag.tsx
@@ -1,26 +1,13 @@
-import cls from "classnames";
+import type { MergeElementProps } from "@styleless-ui/react";
 import * as React from "react";
 import IconButton from "../IconButton";
 import { CloseIcon } from "../internals";
+import type { CommonProps } from "../types";
+import { combineClasses as cls, componentWithForwardedRef } from "../utils";
 import classes from "./Tag.module.css";
+import * as Slots from "./slots";
 
-interface OwnProps {
-  /**
-   * The className applied to the component.
-   */
-  className?: string;
-  /**
-   * The color of the tag.
-   * @default "neutral"
-   */
-  color?:
-    | "neutral"
-    | "primary"
-    | "secondary"
-    | "success"
-    | "error"
-    | "warning"
-    | "info";
+type OwnProps = Pick<CommonProps, "className" | "color"> & {
   /**
    * The content of the tag.
    */
@@ -28,18 +15,17 @@ interface OwnProps {
   /**
    * If provided, the tag will have a delete button.
    */
-  remove?: { callback: React.MouseEventHandler; screenReaderLabel: string };
+  remove?: { onClick: React.MouseEventHandler; screenReaderLabel: string };
   /**
    * The leading icon element placed before the text.
    */
   icon?: React.ReactNode;
-}
+};
 
 export type Props = Omit<
-  React.ComponentPropsWithRef<"div">,
-  keyof OwnProps | "children"
-> &
-  OwnProps;
+  MergeElementProps<"div", OwnProps>,
+  "children" | "defaultValue" | "value" | "checked" | "defaultChecked"
+>;
 
 const TagBase = (props: Props, ref: React.Ref<HTMLDivElement>) => {
   const {
@@ -51,38 +37,57 @@ const TagBase = (props: Props, ref: React.Ref<HTMLDivElement>) => {
     ...otherProps
   } = props;
 
+  const renderIcon = () => {
+    if (!icon) return null;
+
+    return (
+      <div
+        aria-hidden="true"
+        data-slot={Slots.Icon}
+        className={classes.icon}
+      >
+        {icon}
+      </div>
+    );
+  };
+
+  const renderRemove = () => {
+    if (!remove) return null;
+
+    return (
+      <IconButton
+        as="button"
+        size="small"
+        color={color}
+        variant="inlined"
+        className={classes.delete}
+        label={{ screenReaderLabel: remove.screenReaderLabel }}
+        onClick={remove.onClick}
+        icon={<CloseIcon />}
+        data-slot={Slots.Delete}
+      />
+    );
+  };
+
   return (
     <div
       {...otherProps}
       ref={ref}
-      data-slot="Tag:Root"
+      data-slot={Slots.Root}
       className={cls(className, classes.root, classes[`root--${color}`])}
     >
-      {icon && (
-        <div data-slot="Tag:Icon" className={classes.icon}>
-          {icon}
-        </div>
-      )}
-      <span data-slot="Tag:Text" className={classes.text}>
+      {renderIcon()}
+      <span
+        data-slot={Slots.Text}
+        className={classes.text}
+      >
         {text}
       </span>
-      {remove && (
-        <IconButton
-          data-slot="Tag:Delete"
-          as="button"
-          size="small"
-          color={color}
-          variant="inlined"
-          className={classes.delete}
-          label={{ screenReaderLabel: remove.screenReaderLabel }}
-          onClick={remove.callback}
-          icon={<CloseIcon />}
-        />
-      )}
+      {renderRemove()}
     </div>
   );
 };
 
-const Tag = React.forwardRef(TagBase) as typeof TagBase;
+const Tag = componentWithForwardedRef(TagBase, "Tag");
 
 export default Tag;
