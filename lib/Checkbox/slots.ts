a1cc02233eeeb5c7e8df14a0d248a9138ed42ef8
mimshins <mostafa.sh.coderino@gmail.com>
2024-04-30 20:46:08 +0330

    Remove unused slots

--- lib/Checkbox/slots.ts
+++ lib/Checkbox/slots.ts
@@ -1,5 +1,4 @@
 export const Root = "Checkbox:Parent:Root";
 export const Container = "Checkbox:Container";
 export const CheckIndicator = "Checkbox:CheckIndicator";
-export const RequiredIndicator = "Checkbox:RequiredIndicator";
 export const FeedbackMessage = "Checkbox:FeedbackMessage";
--- lib/Radio/slots.ts
+++ lib/Radio/slots.ts
@@ -1,5 +1,4 @@
 export const Root = "Radio:Parent:Root";
 export const Container = "Radio:Container";
 export const CheckIndicator = "Radio:CheckIndicator";
-export const RequiredIndicator = "Radio:RequiredIndicator";
 export const FeedbackMessage = "Radio:FeedbackMessage";
