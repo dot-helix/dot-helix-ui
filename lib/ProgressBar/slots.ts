0dd5b695e8d0ae01ef4917070cacf65d173a97a5
mimshins <mostafa.sh.coderino@gmail.com>
2024-05-04 23:51:48 +0330

    Add meter and progressbar components

---
+++ lib/ProgressBar/slots.ts
@@ -0,0 +1,5 @@
+export const Heading = "ProgressBar:Heading";
+export const Title = "ProgressBar:Heading:Title";
+export const Value = "ProgressBar:Heading:Value";
+export const Track = "ProgressBar:Track";
+export const Fill = "ProgressBar:Track:Fill";
