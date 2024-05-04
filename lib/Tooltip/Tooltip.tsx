ed3c01f9cee589e543f2480d9bd7c62482411a3a
mimshins <mostafa.sh.coderino@gmail.com>
2024-05-04 20:09:49 +0330

    Refactor tooltip component

--- lib/Tooltip/Tooltip.tsx
+++ lib/Tooltip/Tooltip.tsx
@@ -1,55 +1,85 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { Tooltip as StylelessTooltip, TooltipProps } from "@styleless-ui/react";
+import {
+  Tooltip as StylelessTooltip,
+  type MergeElementProps,
+  type TooltipProps,
+} from "@styleless-ui/react";
+import { setRef, useEventCallback } from "@styleless-ui/react/utils";
 import cls from "classnames";
 import * as React from "react";
+import type { CommonProps } from "../types";
+import { componentWithForwardedRef } from "../utils";
 import classes from "./Tooltip.module.css";
 
 type OwnProps = Pick<
   TooltipProps,
+  | "resolveAnchor"
   | "placement"
   | "autoPlacement"
   | "behavior"
   | "open"
   | "defaultOpen"
   | "onOutsideClick"
-> & {
-  /**
-   * The className applied to the component.
-   */
-  className?: string;
-  /**
-   * The text of the tooltip.
-   */
-  text: string;
-  /**
-   * The anchor element for the tooltip.
-   * It's either HTMLElement RefObject, HTMLElement or query selector.
-   */
-  anchorElement: React.RefObject<HTMLElement> | HTMLElement | string;
-};
+> &
+  Pick<CommonProps, "className"> & {
+    /**
+     * The text of the tooltip.
+     */
+    text: string;
+  };
 
 export type Props = Omit<
-  React.ComponentPropsWithRef<"div">,
-  | keyof OwnProps
+  MergeElementProps<"div", OwnProps>,
+  | "value"
   | "defaultValue"
+  | "checked"
   | "defaultChecked"
   | "children"
   | "content"
-  | "placeholder"
-> &
-  OwnProps;
+  | "color"
+>;
 
 const TooltipBase = (props: Props, ref: React.Ref<HTMLDivElement>) => {
-  const { className, anchorElement, text, ...otherProps } = props;
+  const { className, text, ...otherProps } = props;
+
+  const [keepMounted, setKeepMounted] = React.useState(false);
+  const [isVisible, setIsVisible] = React.useState(false);
+
+  const handleTransitionEnd = useEventCallback<
+    React.TransitionEvent<HTMLDivElement>
+  >(event => {
+    if (event.propertyName !== "visibility") return;
+
+    const root = event.currentTarget as HTMLDivElement;
+    const isOpen = root.hasAttribute("data-open");
+
+    if (!isOpen) setKeepMounted(false);
+    else setKeepMounted(true);
+  });
+
+  const refCallback = React.useCallback(
+    (node: HTMLDivElement | null) => {
+      setRef(ref, node);
+
+      if (!node) return;
+
+      const isOpen = node.hasAttribute("data-open");
+
+      if (isOpen) setIsVisible(true);
+      else setIsVisible(false);
+    },
+    [ref],
+  );
 
   return (
     <StylelessTooltip
       {...otherProps}
-      ref={ref}
-      anchorElement={anchorElement}
-      className={({ openState, placement }) =>
+      keepMounted={keepMounted}
+      ref={refCallback}
+      onTransitionEnd={handleTransitionEnd}
+      className={({ open, placement }) =>
         cls(className, classes.root, classes[`root--${placement}`], {
-          [classes[`root--open`]!]: openState,
+          [classes[`root--open`]!]: open,
+          [classes["root--visible"]!]: isVisible,
         })
       }
     >
@@ -58,6 +88,6 @@ const TooltipBase = (props: Props, ref: React.Ref<HTMLDivElement>) => {
   );
 };
 
-const Tooltip = React.forwardRef(TooltipBase) as typeof TooltipBase;
+const Tooltip = componentWithForwardedRef(TooltipBase, "Tooltip");
 
 export default Tooltip;
