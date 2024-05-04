fa255387a429dceeb3857fd4016b94c4f2e68ec5
mimshins <mostafa.sh.coderino@gmail.com>
2024-05-04 21:04:55 +0330

    Minify css

--- scripts/minify-package.ts
+++ scripts/minify-package.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+import CleanCSS from "clean-css";
 import glob from "fast-glob";
 import * as fs from "node:fs/promises";
 import * as path from "node:path";
@@ -7,7 +9,9 @@ const packagePath = process.cwd();
 
 const distPath = path.join(packagePath, "./dist");
 
-const minifyPackageFiles = async (files: string[]) => {
+const minifyJS = async () => {
+  const files = await glob(path.join(distPath, "**/*.js"));
+
   for (const file of files) {
     const isESModule = path.relative(distPath, file).split("/")[0] === "esm";
     const isIndex = path.basename(file) === "index.js";
@@ -31,8 +35,30 @@ const minifyPackageFiles = async (files: string[]) => {
   }
 };
 
-void (async () => {
-  const files = await glob(path.join(distPath, "**/*.js"));
+const minifyCSS = async () => {
+  const files = await glob(path.join(distPath, "**/*.css"));
 
-  await minifyPackageFiles(files);
+  const cssCleaner = new CleanCSS();
+
+  for (const file of files) {
+    const source = await fs.readFile(file, { encoding: "utf8" });
+
+    const result = cssCleaner.minify(source);
+
+    if (result.warnings.length > 0) {
+      console.warn(result.warnings.join("\n"));
+    }
+
+    if (result.errors.length > 0) {
+      console.error(result.errors.join("\n"));
+
+      process.exit(1);
+    }
+
+    await fs.writeFile(file, result.styles);
+  }
+};
+
+void (async () => {
+  await Promise.all([minifyJS(), minifyCSS()]);
 })();
